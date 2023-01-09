d1f4a7ad34862733f5da4380e369535928a78955
safahaimour <98284222+safahaimour@users.noreply.github.com>
2023-01-09 12:17:40 +0200

    update

--- src/xyz_sumaya_again/MyClass.java
+++ src/xyz_sumaya_again/MyClass.java
@@ -2,7 +2,9 @@ package xyz_sumaya_again;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -13,11 +15,29 @@ public class MyClass {
 		WebDriver driver;
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
 		driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
 		driver.manage().window().maximize();
 
+		driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/div[2]/button")).click();
+
+		driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/button[1]")).click();
+
+		String[] myListOfFirstName = { "ahmad", "ali", "marwa", "anas", };
+
+		int randomOne = (int) (Math.random() * myListOfFirstName.length - 1);
+
+		String[] myListOfLastName = { "omar", "mahmoud", "eman", "enas", };
+
+		int randomTwo = (int) (Math.random() * myListOfLastName.length - 1);
+
+		WebElement myInputForTheFirstName = driver
+				.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[1]/input"));
+		myInputForTheFirstName.sendKeys(myListOfFirstName[randomOne]);
+		
+		//WebElement myInputForTheLastName = driver.findElement(By.xpath(""))
 	}
 
 }
